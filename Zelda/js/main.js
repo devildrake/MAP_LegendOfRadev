4aaa0a5ace788b0b96ca83550b8bc0099d8a05f4
Rattaspi <a.canutperdigon@gmail.com>
2017-11-08 00:23:48 +0100

    Hecha la escena de sala secreta 1 y programada en un script. Hecha la estructura basica del programa.

--- Zelda/js/main.js
+++ Zelda/js/main.js
@@ -1,5 +1,5 @@
 var zelda = zelda || {};
 
-zelda.game = new Phaser.Game(256,512,Phaser.AUTO, null, this, false, false);
-zelda.game.state.add("main", zelda.gameState);
+zelda.game = new Phaser.Game(512,512,Phaser.AUTO, null, this, false, false);
+zelda.game.state.add("main", zelda.sala_secreta_1);
 zelda.game.state.start("main");
---
+++ Zelda/js/sala_secreta_1.js
@@ -0,0 +1,43 @@
+var zelda = zelda || {};
+//16X11 tiles
+zelda.sala_secreta_1 = {
+    preload:function(){
+        //this.game.stage.backgroundColor = "#ff0000";
+        this.game.stage.backgroundColor = "#black";
+        this.load.tilemap("sala1", "json/Sala_Secreta.json", null, Phaser.Tilemap.TILED_JSON);
+        this.load.image("secret_tile", "img/secret_tile.png");
+        this.load.spritesheet("fuego", "img/fuego.png",17,15);
+        this.load.spritesheet("npc", "img/oldman.png",16,16);
+        this.load.image("pocion","img/pocion_vida.png");
+        this.load.image("corazon","img/slot_corazon.png");
+    },
+    
+    create:function(){
+        this.map = this.game.add.tilemap("sala1");
+        this.map.addTilesetImage("secret_tile");
+        this.map.createLayer("Rocas");
+        
+        //fuego y animacion de este
+        this.fire = [];
+        this.fire.push(this.game.add.sprite(5*16,4*16,"fuego",0));
+        this.fire.push(this.game.add.sprite(10*16,4*16,"fuego",0));
+        for(var i in this.fire){
+            this.fire[i].animations.add("idle",[0,1],6,true);
+            this.fire[i].animations.play("idle");
+        }
+        
+        //npc
+        this.npc = this.game.add.sprite(8*16, 5*16, "npc", 0);
+        this.npc.anchor.setTo(.5);
+        this.npc.animations.add("idle", [0,1], 2, true);
+        this.npc.animations.play("idle");
+        
+        //objetos
+        this.corazon = this.game.add.sprite(6*16, 6*16,"corazon");
+        this.pocion = this.game.add.sprite(9*16, 6*16, "pocion");
+    },
+    
+    update:function(){
+        
+    }
+};
