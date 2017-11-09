c47b8c009a9f21b50794d74b20b6c7a9bc26930e
Unknown <a.canutperdigon@gmail.com>
2017-11-09 22:59:47 +0100

    A medias la sala secreta 2, tengo que mirar cuales son realmente las que tengo que meter y donde estan exactamente.

--- Zelda/js/sala_secreta_2.js
+++ Zelda/js/sala_secreta_2.js
@@ -1,28 +1,23 @@
-var zelda = zelda || {};
-//16X11 tiles
-zelda.sala_secreta_1 = {
-	init:function(){
+var zelda = zelda || {}
+
+zelda.sala_secreta_2 = {
+    init:function(){
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		this.scale.setGameSize(512/2,480/2);
 	},
-	
+    
     preload:function(){
-        //this.game.stage.backgroundColor = "#ff0000";
         this.game.stage.backgroundColor = "#black";
-        this.load.tilemap("sala1", "json/Sala_Secreta.json", null, Phaser.Tilemap.TILED_JSON);
+        this.load.tilemap("sala", "json/Sala_Secreta.json", null, Phaser.Tilemap.TILED_JSON);
         this.load.image("secret_tile", "img/tilesets/secret_tile.png");
         this.load.spritesheet("fuego", "img/fuego.png",17,15);
-        this.load.spritesheet("npc", "img/oldman.png",16,16);
-        this.load.image("pocion","img/pocion_vida.png");
-        this.load.image("corazon","img/slot_corazon.png");
+        this.load.spritesheet("npc", "img/oldman.png",16, 16);
     },
     
     create:function(){
-		this.allElements=[];
-        this.map = this.game.add.tilemap("sala1");
+        this.map = this.game.add.tilemap("sala");
         this.map.addTilesetImage("secret_tile");
         this.map.createLayer("Rocas");
-		this.allElements.push(this.map);
         
         //fuego y animacion de este
         this.fire = [];
@@ -36,13 +31,9 @@ zelda.sala_secreta_1 = {
         //npc
         this.npc = this.game.add.sprite(8*16, 4*16, "npc", 0);
         this.npc.anchor.setTo(.5,0);
-        
-        //objetos
-        this.corazon = this.game.add.sprite(6*16, 6*16,"corazon");
-        this.pocion = this.game.add.sprite(9*16, 6*16, "pocion");
     },
     
     update:function(){
         
     }
-};
+}
