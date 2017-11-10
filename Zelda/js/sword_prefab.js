0f31d82dfe57507c10b1edeac55d2fc894e50d6a
Devildrake <divadarrei@gmail.com>
2017-11-10 12:44:35 +0100

    Link ya saca la espada al atacar (HAY QUE HACER QUE EL PROYECTIL SEA LA ESPADA NO LA FLECHA)

--- Zelda/js/sword_prefab.js
+++ Zelda/js/sword_prefab.js
@@ -2,9 +2,13 @@ var zelda = zelda || {};
 
 zelda.swordPrefab = function(game,x,y){
 	Phaser.Sprite.call(this,game,x,y,"Sword");
+    
+
+    
 	this.anchor.setTo(.5);
 	this.checkWorldBounds = true;
 	this.outOfBoundsKill = true;
+    console.log("Bruh");
 };
 
 zelda.swordPrefab.prototype = Object.create(Phaser.Sprite.prototype);
