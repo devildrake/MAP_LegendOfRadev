9f3af12d4ecaf65d88a9b97ae2a882131f8a543d
Unknown <a.canutperdigon@gmail.com>
2017-11-18 22:04:36 +0100

    Añadido link en la sala secreta M

--- Zelda/js/sala_secreta_M.js
+++ Zelda/js/sala_secreta_M.js
@@ -20,13 +20,19 @@ zelda.sala_secreta_M = {
         this.load.image("llave", "img/llave.png");
         this.load.image("vela", "img/vela.png");
 		this.load.image("inventario", "img/inventario.png");
+		
+		//para el prefab de link
+		this.load.spritesheet("Link", "img/Link_SpriteSheet.png",16,16); this.load.image("LinkCollider","img/Link/LinkCollider.png");
+        this.load.spritesheet("swordProjectile","img/arrow.png",16,16);
+        this.load.spritesheet("Sword","img/Swords.png",16,16);
     },
     
     create:function(){
         //--------Pintado del layout base------------
         this.map = this.game.add.tilemap("sala");
         this.map.addTilesetImage("secret_tile");
-        this.map.createLayer("Rocas");
+        this.obstacles = this.map.createLayer("Rocas");
+		this.map.setCollisionBetween(1,2,true,"Rocas");
         
         //fuego y animacion de este
         this.fire = [];
@@ -49,6 +55,8 @@ zelda.sala_secreta_M = {
         this.vela = this.game.add.sprite(zelda.secretLayout.item3X, zelda.secretLayout.itemY, "vela");
 		
 		this.game.camera.y -= 47;
+		
+		this.link = new zelda.LinkPrefab(this.game,zelda.gameOptions.gameWidth/2,zelda.gameOptions.gameHeight-60,this);
 		
 		this.inventario = this.game.add.sprite(0,-zelda.gameOptions.gameHeight+47, "inventario");
         this.inventario.fixedToCamera = true;
