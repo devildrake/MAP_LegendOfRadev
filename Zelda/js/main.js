0962b89a46ad0b2b150c427d8bbce5b9f714f405
Unknown <a.canutperdigon@gmail.com>
2017-11-19 23:29:27 +0100

    Actualizado skype y animaciones de spawn de la sala secreta A

--- Zelda/js/main.js
+++ Zelda/js/main.js
@@ -287,4 +287,4 @@ zelda.game.state.add("dungeon", zelda.dungeon);
 
 //Escena que se pinta
 //zelda.game.state.start("main");
-zelda.game.state.start("sword_room");
+zelda.game.state.start("secret_room_A");
