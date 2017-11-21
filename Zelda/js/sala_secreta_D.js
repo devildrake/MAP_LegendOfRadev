d2a5400266cb7f911555677dbbc14e722b23b025
Unknown <a.canutperdigon@gmail.com>
2017-11-22 00:45:01 +0100

    Animaciones en la sala secreta D

--- Zelda/js/sala_secreta_D.js
+++ Zelda/js/sala_secreta_D.js
@@ -13,8 +13,8 @@ zelda.sala_secreta_D = {
         this.load.tilemap("sala", "json/Sala_Secreta.json", null, Phaser.Tilemap.TILED_JSON);
         this.load.image("secret_tile", "img/tilesets/secret_tile.png");
         //-------------------------------------------
-        this.load.spritesheet("fuego", "img/fuego.png",17,15);
-        this.load.spritesheet("npc", "img/oldman.png",16, 16);
+        this.load.spritesheet("fuego", "img/spawn_fuego.png",16,16);
+        this.load.spritesheet("npc", "img/spawn_oldman.png",16, 16);
 		this.load.image("inventario", "img/inventario.png");
 		
 		//para el prefab de link
@@ -30,17 +30,27 @@ zelda.sala_secreta_D = {
 		this.map.setCollisionBetween(1,2,true,"Rocas");
         
         //fuego y animacion de este
-        this.fire = [];
-        this.fire.push(this.game.add.sprite(5*16,4*16,"fuego",0));
-        this.fire.push(this.game.add.sprite(10*16,4*16,"fuego",0));
-        for(var i in this.fire){
-            this.fire[i].animations.add("idle",[0,1],6,true);
-            this.fire[i].animations.play("idle");
-        }
+        this.fire1 = this.game.add.sprite(zelda.secretLayout.fireX1,zelda.secretLayout.fireY,"fuego",0);
+		this.fire1.animations.add("spawn",[0,1,2],6,false);
+		this.fire1.animations.add("idle",[3,4],6,true);
+        this.fire1.animations.play("spawn");
+		this.fire1.animations.currentAnim.onComplete.add(function () {
+			this.fire1.animations.play("idle");
+		},this);
+		
+        this.fire2 = this.game.add.sprite(zelda.secretLayout.fireX2,zelda.secretLayout.fireY,"fuego",0);
+		this.fire2.animations.add("spawn",[0,1,2],6,false);
+		this.fire2.animations.add("idle",[3,4],6,true);
+        this.fire2.animations.play("spawn");
+		this.fire2.animations.currentAnim.onComplete.add(function () {
+			this.fire2.animations.play("idle");
+		},this);
         
         //npc
         this.npc = this.game.add.sprite(8*16, 4*16, "npc", 0);
         this.npc.anchor.setTo(.5,0);
+		this.npc.animations.add("spawn",[0,1,2,3],6, false);
+		this.npc.animations.play("spawn");
 		
 		this.game.camera.y -= 47;
 		
