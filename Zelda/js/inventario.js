5795937d3c43073c7948b37bfccc15978ca492fa
Zalexe <carlosmarruedo@hotmail.com>
2017-11-23 00:18:00 +0100

    push q no pude hacer a la mañana

--- Zelda/js/inventario.js
+++ Zelda/js/inventario.js
@@ -8,12 +8,10 @@ zelda.InventarioPrefab=function(game,x,y,level) {
        this.inv = this.game.add.sprite(0,0, "background");
 		
         //posiciones
-        this.positionObj1=125;
-        this.positionObj2=145;
-        this.positionObj3=165;
-        this.positionObj4=185;
-        this.positionObj5=200;
-        this.InvON= false;
+        this.ArrayPosObjX=[125,145,165,185,200
+                          ,125,145,165,185,200];
+       
+        
         this.row1=50;
         this.row2=67;
         
@@ -23,27 +21,68 @@ zelda.InventarioPrefab=function(game,x,y,level) {
        
 		
 		this.game.camera.y = 0;
-		
+		this.InvON= false;
+        
         //para saber si ya hemos puesto el objeto en el inventario
         this.CreateBombs=false;
         this.CreateVela=false;
         this.CreatePotion=false;
         
-        
+    this.draw=function{
         for( var i=0;i<array.size();i++){
             
-            
+            //bombas
             if(zelda.Inventory.objects[1]==1 && this.CreateBombs==false ){
                 
                 
+                if(i<5){
+                this.bomb = this.game.add.sprite(this.ArrayPosObjX[i],this.row1, "bomba");
+                }
+                else{
+                    this.bomb = this.game.add.sprite(this.ArrayPosObjX[i],this.row2, "bomba");
+                }
+                this.CreateBombs=true;
+                this.array[i]="bombs";
+                console.log("pintada")
             }
+            //vela
+            else if(zelda.Inventory.objects[3]==1 && this.CreateVela==false){
+                
+                
+                
+                if(i<5){
+                this.vela = this.game.add.sprite(this.ArrayPosObjX[i],this.row1, "vela");
+                }
+                else{
+                    this.vela = this.game.add.sprite(this.ArrayPosObjX[i],this.row2, "vela");
+                }
+                
+                this.CreateVela=true;
+                this.array[i]="vela";
+            }
+            
+            //Potion
+              else if(zelda.Inventory.objects[4]==1 && this.CreatePotion==false){
+                
+                
+                
+                if(i<5){
+                this.vela = this.game.add.sprite(this.ArrayPosObjX[i],this.row1, "potion");
+                }
+                else{
+                    this.vela = this.game.add.sprite(this.ArrayPosObjX[i],this.row2, "potion");
+                }
+                
+                this.CreatePotion=true;
+                  this.array[i]="potion";
+            }
+            
+            //objeto
         }
-        this.bomb = this.game.add.sprite(this.positionObj1,67, "bomba");
-        this.bombs = this.game.add.sprite(this.positionObj2,50, "bomba");
-		this.bombd = this.game.add.sprite(this.positionObj3,50, "bomba");
-		
         
-    },
+    }
+        
+        }    ,
     
         zelda.InventarioPrefab.prototype = Object.create(Phaser.Sprite.prototype);
 
