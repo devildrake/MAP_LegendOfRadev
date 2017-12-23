461c55feddc6248eb5b8d389f154818ff55bb899
Zalexe <carlosmarruedo@hotmail.com>
2017-12-23 18:58:20 +0100

    heart container operativo

--- Zelda/js/HeartContainerPrefab.js
+++ Zelda/js/HeartContainerPrefab.js
@@ -19,8 +19,55 @@ zelda.HeartContainerPrefab.prototype.update = function(){
     if(this.Alive){
     this.game.physics.arcade.overlap(this,this.level.linkInstance,
         function(heartContainer,linkInstance){
+        //eliminar los corazones de antes
+        for(var i=0;i< zelda.LinkObject.maxHearts;i++){
+            zelda.Inventory.hearts[i].destroy();
+         }
+        for(var i=0;i< zelda.LinkObject.currentHearts;i++){
+            if(zelda.LinkObject.currentHearts==1 || zelda.LinkObject.currentHearts==2 || zelda.LinkObject.currentHearts==3 || zelda.LinkObject.currentHearts==4 || zelda.LinkObject.currentHearts==5){
+                zelda.Inventory.heart[i].destroy();
+            }else{
+                if(i==zelda.LinkObject.currentHearts-.5){
+                    zelda.Inventory.heart[i].destroy();
+
+
+                }else{
+                    zelda.Inventory.heart[i].destroy();
+                }                   
+            }
+        }
+        
         zelda.LinkObject.maxHearts++;
         zelda.LinkObject.currentHearts = zelda.LinkObject.maxHearts;
+        zelda.Inventory.heart=[zelda.LinkObject.maxHearts];
+        zelda.Inventory.hearts=[zelda.LinkObject.maxHearts];
+        
+        //pintar los corazones
+        for(var i=0;i< zelda.LinkObject.maxHearts;i++){
+            zelda.Inventory.hearts[i]=zelda.game.add.sprite(zelda.game.camera.x+50*i,zelda.game.camera.y+20, "emptyHeart");
+            zelda.Inventory.hearts[i].position.x=zelda.game.camera.x+175+10*i;
+            zelda.Inventory.hearts[i].position.y=zelda.game.camera.y+15;
+         }
+        for(var i=0;i< zelda.LinkObject.currentHearts;i++){
+            if(zelda.LinkObject.currentHearts==1 || zelda.LinkObject.currentHearts==2 || zelda.LinkObject.currentHearts==3 || zelda.LinkObject.currentHearts==4 || zelda.LinkObject.currentHearts==5){
+                zelda.Inventory.heart[i]=zelda.game.add.sprite(zelda.game.camera.x+50*i,zelda.game.camera.y+20, "corazon");
+                zelda.Inventory.heart[i].position.x=zelda.game.camera.x+175+10*i;
+                zelda.Inventory.heart[i].position.y=zelda.game.camera.y+15;
+            }else{
+                if(i==zelda.LinkObject.currentHearts-.5){
+                    zelda.Inventory.heart[i]=zelda.game.add.sprite(zelda.game.camera.x+50*i,zelda.game.camera.y+20, "HalfHeart");
+                    zelda.Inventory.heart[i].position.x=zelda.game.camera.x+175+10*i;
+                    zelda.Inventory.heart[i].position.y=zelda.game.camera.y+15;
+
+
+                }else{
+                    zelda.Inventory.heart[i]=zelda.game.add.sprite(zelda.game.camera.x+50*i,zelda.game.camera.y+20, "corazon");
+                    zelda.Inventory.heart[i].position.x=zelda.game.camera.x+175+10*i;
+                    zelda.Inventory.heart[i].position.y=zelda.game.camera.y+15;
+                }                   
+            }
+        }
+        
         heartContainer.kill();
         zelda.LinkPrefab.grabItemSound.play();
         this.Alive = false;
